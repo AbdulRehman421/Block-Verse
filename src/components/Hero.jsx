d6e68978c5520b207aae7c5d1664da4424353d7f
AbdulRehman421 <rkofficial0910@gmail.com>
2022-12-17 20:05:03 +0530

    Hero section margin reduced

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <p className=' mx-5 mt-5 px-5 pt-5 text-slate-200'>
                     Empowers traders, liquidity providers & developers to participate in an open financial marketplace with no barriers to entry.
                 </p>
-                <div className='mx-5 mt-5 px-5 pt-5 flex justify-evenly'>
+                <div className='mx-5 md:mt-5 md:px-5 pt-5 flex justify-evenly'>
                     <Link to='/marketplace'>
                         <button className=' bg-gradient-to-r from-violet-500 to-cyan-500 py-2 px-7 rounded hover:from-transparent hover:to-transparent border border-black hover:border-t-violet-400 hover:border-l-violet-300 hover:border-b-teal-400 hover:border-r-teal-200  '>
                             Explore
@@ -25,7 +25,7 @@ const Hero = () => {
                 </div>
             </div>
 
-            <div className=' mt-5 md:w-1/2 md:mt-0'>
+            <div className=' md:w-1/2'>
                 <img src='/images/hero.svg' alt='' className=' ' />
             </div>
         </div>
